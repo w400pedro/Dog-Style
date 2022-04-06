e3dc0577c0a43f08cf267ea65273fb3694fd12c4
w400pedro <pepepadilha12@hotmail.com>
2022-04-06 04:17:08 -0300

    commited

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './view');
 
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use(express.json());
+
+const session = require('express-session');
+app.use(session({
+    secret: 'chave secreta de criptografia',
+    resave: false, 
+    saveUninitialized: false,
+    cookie: { secure: false }
+}))
+
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.redirect('/dogs');
@@ -12,4 +28,7 @@ app.get('/', (req, res) => {
 
 const dogroutes = require('./routes/dog-routes');
 app.use('/dogs', dogroutes);
+
 app.listen(3000, function () { console.log("entrou") });
+
+//Duvidas pra perguntar pro prof hiihihih. Precisa fazer um sistema de cadastro/login? pode usar banco? 
