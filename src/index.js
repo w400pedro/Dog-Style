a7ec49f750a217db14426804e7d2f95ce3ac1495
w400pedro <pepepadilha12@hotmail.com>
2022-04-06 06:09:55 -0300

    adição da pasta SRC

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', './view');
+app.set('views', './src/view');
 
 app.use(express.urlencoded({
     extended: true,
