2b2b074843e6312ae91a8fafaa07e255d11aa2b2
w400pedro <pepepadilha12@hotmail.com>
2022-04-07 04:40:38 -0300

    insert patch

--- src/controllers/dog-controllers.js
+++ src/controllers/dog-controllers.js
@@ -1,27 +1,44 @@
 const dog = require('../database/dogs-db');
+const { nanoid } = require('nanoid');
 
 class dogFunction {
-async showDog(req, res){
-    //AQUI TU BOTA QUE SÓ VAI EXECUTAR A AÇÃO ABAIXO SE O USER TIVER LOGADO
-   res.render('dog-list', { dogs: dog});
-};
+    async showDog(req, res) {
+        //AQUI TU BOTA QUE SÓ VAI EXECUTAR A AÇÃO ABAIXO SE O USER TIVER LOGADO
+        res.render('dog-list', { dogs: dog });
+    };
 
-async showDetails (req, res) {
-    const { id } = req.params;
-    const dogfiltrado = dog.filter(value => value.id == id);
-    res.render('dog-details', { dogs: dogfiltrado[0]});
-}
+    async showDetails(req, res) {
+        const { id } = req.params;
+        const dogfiltrado = dog.filter(value => value.id == id);
+        res.render('dog-details', { dogs: dogfiltrado[0] });
+    }
 
-async deleteDog(req, res){
-const { id } = req.params;
-    res.send({id})
-};
+    async showCreateDog(req, res) {
+        //COLOCAR AQUI QUE SÓ O ADM TEM AUTORIZAÇÃO
+        res.render('dog-create');
+    };
 
-async updateDog(req, res){
-    const { id } = req.params;
-    res.send({id});
-}
+    async deleteDog(req, res) {
+        //COLOCAR AQUI QUE SÓ O ADM TEM AUTORIZAÇÃO
+        const { id } = req.params;
 
+    };
+
+    async updateDog(req, res) {
+        //COLOCAR AQUI QUE SÓ O ADM TEM AUTORIZAÇÃO
+        const { id } = req.params;
+        res.send({ id });
+    }
+
+    async createDog(req, res) {
+        //COLOCAR AQUI QUE SÓ O ADM TEM AUTORIZAÇÃO 
+        dog.push({
+            id: nanoid(8),
+            ...req.body
+        });
+
+        res.redirect('/dogs');
+    }
 
 }
 
--- src/routes/dog-routes.js
+++ src/routes/dog-routes.js
@@ -4,11 +4,14 @@ const router = Router();
 const { dogFunction } = require('../controllers/dog-controllers');
 
 
-const funcaodog = new dogFunction
+const funcaodog = new dogFunction;
 
 router.get('/', funcaodog.showDog);
-router.get('/:id', funcaodog.showDetails)
+router.get('/details/:id', funcaodog.showDetails);
+router.get('/create', funcaodog.showCreateDog);
 router.get('/delete/:id', funcaodog.deleteDog);
 router.get('/update/:id', funcaodog.updateDog);
+
+router.post('/', funcaodog.createDog);
 
 module.exports = router;
