942656f26d1e9cab231a3a5e56a8875c5ef7552f
Pedro <pedrogarciaifrs@gmail.com>
2022-04-07 11:13:06 -0300

    ai papi

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -8,7 +8,7 @@ const funcaouser = new userFunction;
 
 router2.get('/', funcaouser.showProfile);
 
-router2.post('/login', usersController.login);
+//router2.post('/login', user-controllers.login);
 
 router2.get('/register', funcaouser.userRegistration); // CARA QND TU TERMINAR DE FZR TODA AS COISAS DE CADASTRO, TU MUDA ESSE ROUTER2.GET PRA ROUTER2.POST, AI TEUS ARQUIVOS DOS INPUTS VAO SER USADOS COMO POST
 
