2e86bab51fa3dbfc2f6d03cb3daa1349653e3932
Pedro <pedrogarciaifrs@gmail.com>
2022-04-07 11:06:29 -0300

    login k (não testado papi)

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -1,4 +1,4 @@
-//const users = require('../database/user-db'); Descomenta qnd criar o banco
+const users = require('../database/user-db'); //Descomenta qnd criar o banco
 
 class userFunction {
     async showProfile(req, res) {
@@ -9,6 +9,22 @@ class userFunction {
     async userRegistration(req, res){
         // VE A PORRA DA SEMANA 6 Q TU VAI ENTENDER TUDO Q TEM Q FZR AQUI
         res.render('user-profile')
+    }
+
+    async login(req, res) {
+    
+        const { email, senha } = req.body;
+        const usuarioEcontrado = users.find(u => u.email == email);
+
+        if (!usuarioEcontrado) return res.send('Usuario não encontrado');
+
+        if (usuarioEcontrado.senha == senha) {
+            req.session.user = usuarioEcontrado;
+            return res.send('Logado com Sucesso');
+        } else {
+            return res.send('Senha Errada');
+        }
+        
     }
 }
 
--- src/database/user-db.js
+++ src/database/user-db.js
@@ -1,1 +1,7 @@
 //Muito dificil, não sei o que tem que fazer aqui
+const users = [
+    {id: 1, nome: 'PicaEnlatada', email: 'Pica.Enlatada@riogrande.ifrs.edu.br', senha: '123', data_nascimento: '01/01/0001'},
+    {id: 2, nome: 'JorranesBeises', email: 'k@gmail.com', senha: '123', data_nascimento: '11/10/2002'},
+    {id: 3, nome: 'Pepona', email: 'mamaco@gmail.com', senha: 'ugauga', data_nascimento: 'completely unknown to society'}
+]
+module.exports = users; 
