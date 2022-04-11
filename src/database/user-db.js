e6ae01c2d602352e20f95657978de610a3ead06c
w400pedro <pepepadilha12@hotmail.com>
2022-04-10 22:06:41 -0300

    sub-final patch

--- src/database/user-db.js
+++ src/database/user-db.js
@@ -1,6 +1,6 @@
 const users = [
-    {id: 1, nome: 'Papai João', email: 'papai@riogrande.ifrs.edu.br', senha: '123', data_nascimento: '01/01/0001', racafavorita: 'Poodle', adm: true, foto: ''},
-    {id: 2, nome: 'JorranesBeises', email: 'k@gmail.com', senha: '123', data_nascimento: '11/10/2002', racafavorita: '', adm: false , foto: 'https://i.pinimg.com/originals/51/12/f2/5112f2ee724336804c37db4adb4e52e5.jpg'},
+    {id: 1, nome: 'Felipe Santos', email: 'papai@riogrande.ifrs.edu.br', senha: '123', data_nascimento: '01/01/0001', racafavorita: 'Poodle', adm: true, foto: ''},
+    {id: 2, nome: 'Neymar Junior', email: 'neymar@gmail.com', senha: '123', data_nascimento: '11/10/2002', racafavorita: '', adm: false , foto: 'https://conteudo.imguol.com.br/c/esporte/f5/2022/03/31/neymar-em-acao-durante-partida-do-psg-no-campeonato-frances-em-marco-de-2022-1648755247588_v2_3x4.jpg'},
     {id: 3, nome: 'Pepona', email: 'pepona@gmail.com', senha: 'ugauga', data_nascimento: '20/02/2010', racafavorita: '',  adm: false, foto: ''}
 ]
 module.exports = users; 
